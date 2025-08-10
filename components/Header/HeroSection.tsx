bcceae0f35e89190b6cc57bd7b84acacddc69a81
NafisMahmudAyon <nafismahmudayon@gmail.com>
2025-08-10 16:50:52 +0600

    final push

--- components/Header/HeroSection.tsx
+++ components/Header/HeroSection.tsx
@@ -5,12 +5,12 @@ const HeroSection = () => {
   return (
     <div
       id='home'
-      className='mx-auto mt-4 w-full rounded-t-3xl background bg-[#f1f6fb] pb-16 pt-6 shadow shadow-normalText/25 drop-shadow-md backdrop-blur-3xl dark:bg-[#262626] md:min-h-screen lg:max-w-[1440px]'
+      className='mx-auto mt-4 w-full rounded-t-3xl background bg-[#f1f6fb] pb-16 pt-16 lg:pt-6 shadow shadow-normalText/25 drop-shadow-md backdrop-blur-3xl dark:bg-[#262626] md:min-h-screen lg:max-w-[1440px]'
     >
-      <div className='block lg:hidden'>
+      {/* <div className='block lg:hidden'>
 
         <NavBar />
-      </div>
+      </div> */}
       <Hero />
     </div>
   )
